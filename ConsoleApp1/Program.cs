2d238ee74ef04254366eba5251b8fb5101af1378
416Shemi <tu7ngcsvr@code.edu.az>
2022-03-05 10:54:32 +0400

    new projekt

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nambers = { 3, 5, 12, 1, 4, 8,-4  };
+            int min = nambers[0];
+            for (int i = 0; i < nambers.Length; i++)
+                if(nambers[i]<min)
+            {
+                    min = nambers[i];
+
+            }
+
+            {
+                Console.WriteLine(min);
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
